1270d13f9d8c1eb72d8125bd4a3b60ae3b064ac0
Vlasova Marisha <vlasovamarisha2004@gmail.com>
2023-04-29 13:45:40 +0500

    Podschet peresecheniy

--- src/libgeometry/calc.c
+++ src/libgeometry/calc.c
@@ -1,64 +1,104 @@
-
 #include <ctype.h>
-
 #include <math.h>
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
-double FindRadius(char* str)
+#include "libgeometry/calc.h"
 
+void FindInfo(struct FigureInfo* current, char* str)
 {
-    int length = strlen(str);
-
-    char radius[10];
-
-    while (str[length] != ',') {
-        length--;
+    current->circle = str;
+    int length = 0;
+    char xcord[10];
+    char ycord[10];
+    char Radius[10];
+    while (str[length] != '(') {
+        length++;
     }
+    length++;
+    while (str[length] == ' ') {
+        length++;
+    }
+    int cur = 0;
+    while (str[length] != ' ') {
+        xcord[cur] = str[length];
+        length++;
+        cur++;
+    }
+    current->Xcord = atof(xcord);
 
+    while (str[length] == ' ') {
+        length++;
+    }
+    cur = 0;
+    while (str[length] != ',') {
+        ycord[cur] = str[length];
+        length++;
+        cur++;
+    }
+    current->Ycord = atof(ycord);
     length++;
 
     while (str[length] == ' ') {
         length++;
     }
 
-    int current = 0;
+    cur = 0;
+    while (str[length] != ')') {
+        Radius[cur] = str[length];
+        length++;
+        cur++;
+    }
+    current->radius = atof(Radius);
+}
 
-    while (str[length] != ' ' && str[length] != ')') {
-        radius[current] = str[length];
+double perimeter(char* str, struct FigureInfo* current)
+{
+    if (current->radius == 0)
+        FindInfo(current, str);
+    double result = M_PI * current->radius * 2;
+    return result;
+}
 
-        current++;
+double area(char* str, struct FigureInfo* current)
+{
+    if (current->radius == 0)
+        FindInfo(current, str);
+    double result = M_PI * current->radius * current->radius;
+    return result;
+}
 
-        length--;
+int intersections(char* str, struct FigureInfo* current)
+{
+    if (current->radius == 0)
+        FindInfo(current, str);
+    printf("Intersections:\n");
+    if (current->prev == NULL) {
+        printf("Это первый круг, и он ни с чем не может пересекаться\n\n");
+        struct FigureInfo* node = current;
+        current = current->next;
+        current->prev = node;
+        return -1;
     }
 
-    radius[current + 1] = '\0';
-
-    double a = atof(radius);
-
-    return a;
-}
-
-double perimeter(char str[])
-
-{
-    double radius = FindRadius(str);
-
-    double a = M_PI * radius * 2;
-
-    return a;
-}
-
-double area(char str[])
-
-{
-    double radius = FindRadius(str);
-
-    double a = M_PI * radius * radius;
-
+    struct FigureInfo* tail = current;
+    current = current->prev;
+    int a = 0;
+    while (current != NULL) {
+        double dist
+                = sqrt(pow((tail->Xcord - current->Xcord), 2)
+                       + pow((tail->Ycord - current->Ycord), 2));
+        double condition = tail->radius + current->radius;
+        if (condition > dist) {
+            printf("%s\n", current->circle);
+            a++;
+        }
+        current = current->prev;
+    }
+    if (a == 0) {
+        printf("No intersects\n");
+        return 0;
+    }
     return a;
 }
