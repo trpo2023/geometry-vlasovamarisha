24708f5232781649c459d1edc8190f571adba2da
Vlasova Marisha <vlasovamarisha2004@gmail.com>
2023-04-29 13:47:41 +0500

    Add functiu peresecheniy

--- src/libgeometry/calc.h
+++ src/libgeometry/calc.h
@@ -1,5 +1,16 @@
 #pragma once
 
-double perimeter(char* str);
+struct FigureInfo {
+    double Xcord;
+    double Ycord;
+    double radius;
+    struct FigureInfo* next;
+    struct FigureInfo* prev;
+    char* circle;
+};
 
-double area(char* str);
+double perimeter(char* str, struct FigureInfo* current);
+
+double area(char* str, struct FigureInfo* current);
+
+int intersections(char* str, struct FigureInfo* current);
